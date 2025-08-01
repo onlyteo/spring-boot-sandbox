049c959056a6832552c3c0497f14e76f8d999139
Thomas <onlyteo@live.com>
2025-08-01 15:57:49 +0200

    Updated dependencies

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -25,11 +25,11 @@ plugins {
     kotlin("plugin.noarg") version "2.2.0" apply false
     kotlin("plugin.spring") version "2.2.0" apply false
     kotlin("plugin.serialization") version "2.2.0" apply false
-    id("org.springframework.boot") version "3.5.3" apply false
+    id("org.springframework.boot") version "3.5.4" apply false
     id("io.spring.dependency-management") version "1.1.7" apply false
     id("com.google.cloud.tools.jib") version "3.4.5" apply false
     id("org.jooq.jooq-codegen-gradle") version "3.20.5" apply false
-    id("com.gorylenko.gradle-git-properties") version "2.5.0" apply false
+    id("com.gorylenko.gradle-git-properties") version "2.5.2" apply false
     id("com.expediagroup.graphql") version "8.8.1" apply false
 }
 
